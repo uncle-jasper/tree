9de5cc50f510a32fc8773bfa1d6d3c7ff2edecc7
uncle-jasper <79769720+uncle-jasper@users.noreply.github.com>
2026-05-06 04:45:06 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'tree-v22';
+const CACHE = 'tree-v23';
 const ASSETS = ['/tree/', '/tree/index.html', '/tree/manifest.json', '/tree/icon.svg'];
 
 self.addEventListener('install', e => {
